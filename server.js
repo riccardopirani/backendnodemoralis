e520d6a8b943b4f072651de8835c4ba07aa5c74f
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-07-22 17:00:14 +0200

    fix code

--- server.js
+++ server.js
@@ -95,7 +95,6 @@ app.get("/api/nft/:address", async (req, res) => {
   }
 });
 
-// Mint JetCV NFT (solo owner/piattaforma)
 app.post("/api/cv/mint", async (req, res) => {
   const { uri } = req.body;
 
