151e0949b008c63bb20577ca752168765a56ccf4
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-07-30 08:50:09 +0200

    fix code

--- server.js
+++ server.js
@@ -556,7 +556,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "ui", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server avviato su http://localhost:${PORT}`);
 });
