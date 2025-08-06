315d3ba75e4c32724757cacd39cc2da4131fc01a
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-08-06 18:05:19 +0200

    fix code

--- complycubeClient.js
+++ complycubeClient.js
@@ -1,8 +1,7 @@
 import ComplyCube from "@complycube/api";
 
-const complycubeClient = new ComplyCube.Static({
+const complycubeClient = new ComplyCube({
   apiKey:
     "test_NWY3R2ZzY3o2SldtUmJGM3c6N2NmNzYzMzAwNjM2ZjhiZDg0ZmFjODZmMTljNDY3OTk0OWZhZGE5Yjc5ODAyM2U2ZmI4ODA2Y2I3NGVjMWI4Nw==;",
 });
-
 export default complycubeClient;
