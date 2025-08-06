5b8fa91926aabb2dd04f2a17e1dca5ab2a8f972d
Riccardo Pirani <riccardo.pirani@reiser.it>
2025-08-06 16:58:20 +0200

    fix code

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 
 app.use("/api/wallets", walletPrismaRoutes);
@@ -38,7 +38,6 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ANKR_RPC = process.env.ANKR_RPC_URL;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const WEB3_STORAGE_TOKEN = process.env.WEB3_STORAGE_TOKEN;
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 
 if (!ANKR_RPC || !PRIVATE_KEY || !CONTRACT_ADDRESS || !WEB3_STORAGE_TOKEN) {
   console.error("Errore: variabili .env mancanti.");
@@ -65,7 +64,7 @@ function decryptPrivateKey({ iv, encrypted, tag }, secret) {
   const decipher = crypto.createDecipheriv(
     "aes-256-gcm",
     key,
-    Buffer.from(iv, "hex"),
+    Buffer.from(iv, "hex")
   );
   decipher.setAuthTag(Buffer.from(tag, "hex"));
   const decrypted = Buffer.concat([
@@ -77,7 +76,7 @@ function decryptPrivateKey({ iv, encrypted, tag }, secret) {
 
 export async function downloadAndDecryptFromUrl(
   fileUrl,
-  outputName = "cv_decrypted.png",
+  outputName = "cv_decrypted.png"
 ) {
   let encryptionKey;
   try {
@@ -188,7 +187,7 @@ app.get("/api/wallet/:address/balance", async (req, res) => {
 
     // Esegui lo script
     const { stdout, stderr } = await execAsync(
-      `bash ${scriptPath} ${walletId}`,
+      `bash ${scriptPath} ${walletId}`
     );
 
     if (stderr) {
@@ -201,7 +200,7 @@ app.get("/api/wallet/:address/balance", async (req, res) => {
     // L'output di read_secret.sh contiene gli attributi JSON
     // Cerchiamo il nodo specifico "wallet-<ID>"
     const match = stdout.match(
-      new RegExp(`"wallet-${walletId}"\\s*:\\s*"(.*?)"`),
+      new RegExp(`"wallet-${walletId}"\\s*:\\s*"(.*?)"`)
     );
 
     if (!match) {
@@ -302,7 +301,7 @@ app.post("/api/cv/:tokenId/update", async (req, res) => {
   try {
     const ipfsUri = await uploadToWeb3StorageFromUrl(
       newURI,
-      `cv-${Date.now()}.json`,
+      `cv-${Date.now()}.json`
     );
     const tx = await contract.updateTokenURI(user, ipfsUri);
     await tx.wait();
